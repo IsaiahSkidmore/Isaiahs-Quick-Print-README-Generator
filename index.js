10c75bb88a0554413ee3476c0f5cd0a5b6031766
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-08-10 22:39:25 -0700

    added the array of questions and installed the proper depen dencies. I will next be adding the function to write the answers to a README file in the proper format.

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ const questions = [
 
 
 // TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-    
 // TODO: Create a function to initialize app
-
+function init() {}
 
 // Function call to initialize app
 init();
