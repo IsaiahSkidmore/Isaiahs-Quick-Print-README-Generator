d0d62114a5edcbc4b246dcffed3117cd5f18eabc
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-08-10 22:58:31 -0700

    added the code to import both inquirer and fs. Also created the function to write the answers from inquirer to a README document in the proper layout

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -59,10 +61,62 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(answers) {
+   return `
+# ${answers.title}
+
+
+## Description
+${answers.description}
+
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+
+## Installation
+${answers.installation}
+
+
+## Usage
+${answers.usage}
+
+
+## License
+This project is licensed under the ${answers.license} license.
+
+
+## Contributing
+${answers.contributing}
+
+
+## Tests
+${answers.tests}
+
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at [${answers.github}](https://github.com/${answers.github}/).
+`;
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        fs.writeFile("README.md", writeToFile(answers), (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Successfully wrote to README.md");
+        });
+    });
+ }
+ 
 
 // Function call to initialize app
 init();
