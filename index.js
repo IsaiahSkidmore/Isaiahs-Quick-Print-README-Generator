8647c33527031b6d7175d047ad7db3e476b931f1
Isaiah Skidmore <isaiahschardt0617@gmail.com>
2024-08-11 18:35:10 -0700

    added the import/export for the seperate js files and added the code to add the image of the license being used and the markdown generator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
 import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -61,62 +62,24 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(answers) {
-   return `
-# ${answers.title}
-
-
-## Description
-${answers.description}
-
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-
-## Installation
-${answers.installation}
-
-
-## Usage
-${answers.usage}
-
-
-## License
-This project is licensed under the ${answers.license} license.
-
-
-## Contributing
-${answers.contributing}
-
-
-## Tests
-${answers.tests}
-
-
-## Questions
-If you have any questions about the repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at [${answers.github}](https://github.com/${answers.github}/).
-`;
-}
 
 
 // TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        fs.writeFile("README.md", writeToFile(answers), (err) => {
+const init = () => {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        fs.writeFile('README.md', generateMarkdown(answers), (err) => {
             if (err) {
-                return console.log(err);
+                console.log(err);
+            } else {
+                console.log('README.md file has been created successfully!');
             }
-            console.log("Successfully wrote to README.md");
         });
     });
- }
- 
+};
+
+
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+import inquirer from 'inquirer';
+import fs from 'fs';
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+  } else if (license === 'GNU GPLv3') {
+    return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+  } else if (license === 'Apache 2.0') {
+    return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+  } else if (license === 'ISC') {
+    return '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return 'https://opensource.org/licenses/MIT';
+  } else if (license === 'GNU GPLv3') {
+    return 'https://www.gnu.org/licenses/gpl-3.0';
+  } else if (license === 'Apache 2.0') {
+    return 'https://opensource.org/licenses/Apache-2.0';
+  } else if (license === 'ISC') {
+    return 'https://opensource.org/licenses/ISC';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
+const generateMarkdown = (answers) => {
+  return `
+# ${answers.title} ${renderLicenseBadge(answers.license)}
+
+## Description
+${answers.description}
+
+## Installation
+\`\`\`
+${answers.installation}
+\`\`\`
+
+## Usage
+\`\`\`
+${answers.usage}
+\`\`\`
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributors
+${answers.contributors}
+
+## Contact
+If you have any questions, please reach out to ${answers.contact}.
 `;
 }
 
+// Export the generateMarkdown function
 export default generateMarkdown;
